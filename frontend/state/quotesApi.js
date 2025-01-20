72010046a9ba335062be59795cb6e620f01e61dd
BugzeeDev <irvingcsylva@gmail.com>
2025-01-19 23:00:37 -0800

    changed hard coded quotes by wiring the component to the api to fetch quotes

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,14 +1,14 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const quotesApi = createApi({
     reducerPath: 'quotesApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/'}),
     endpoints: builder => ({
         getQuotes: builder.query({
-
+            query: () => 'quotes',
         }),
         createQuote: builder.mutation({
-
+            
         }),
         toggleQuote: builder.mutation({
 
