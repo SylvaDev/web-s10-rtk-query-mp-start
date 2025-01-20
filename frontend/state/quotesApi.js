15fba41d35998818fc0b61c439be49fe066d336b
BugzeeDev <irvingcsylva@gmail.com>
2025-01-19 22:46:51 -0800

    fleshed out the base of the quotesApi

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,24 @@
-// create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const quotesApi = createApi({
+    reducerPath: 'quotesApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/'}),
+    endpoints: builder => ({
+        getQuotes: builder.query({
+
+        }),
+        createQuote: builder.mutation({
+
+        }),
+        toggleQuote: builder.mutation({
+
+        }),
+        deleteQuote: builder.mutation({
+
+        })
+    })
+})
+
+export const {
+    useGetQuotesQuery, useToggleFakeMutation, useCreateQuoteMutation, useDeleteQuoteMutation,
+} = quotesApi
